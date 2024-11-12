975df3f8cc6dc57721128f398052b0fcad30a572
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-11-12 17:52:41 +0600

    Pass new items to the  index page

--- my_market_place/core/views.py
+++ my_market_place/core/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from product.models import Product
+from category.models import Category
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    top_products = Product.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+        'products': top_products, 
+    }
+    return render(request, 'core/index.html', context=context)
 
 def contact(request):
     return render(request, 'core/contact.html')
