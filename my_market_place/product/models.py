6bbbe03090622fecf3542bbc97898eaed9f0e0aa
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-11-12 16:14:56 +0600

    Create `Product` model

--- my_market_place/product/models.py
+++ my_market_place/product/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from category.models import Category
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='product_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name="products", on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
+    
+    
